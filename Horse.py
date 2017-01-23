1531e99d5a75ce982c7195f89f47bf41a2f5ab41
Yenny Yulianny <yulianny.fang@gmail.com>
2017-01-22 22:09:50 -0800

    Update

--- Horse.py
+++ Horse.py
@@ -1,1 +1,82 @@
-bla bah blah
+
+print ("This is Question 1");
+print("------------------------");
+if "Naz" > "Yenny":
+    print "My name is greater!";
+elif "Yenny" > "Naz":
+    print "Their name is greater.";
+else:
+    print "Our name is equal!";
+
+
+
+print ("\n")
+print ("This is Question 2");
+print("------------------------");
+today = 12
+if (31/2) < today:
+    print "oh, we're halfway there!"
+else:
+    print "The month is still young"
+
+
+print ("\n")
+print("This is Question 3");
+print("------------------------");
+today = "Wednesday";
+if today == "Monday":
+    print "Yasss Monday! Here we go!!"
+elif today == "Tuesday":
+    print "Sigh, its only Tuesday!!"
+elif today == "Wednesday":
+    print "Humpday!"
+elif today == "Thursday":
+    print "#Almost there!!"    
+elif today == "Friday":
+    print "TGIF!"    
+else :
+    print "Yeah, it is the weekend!!"
+
+
+print ("\n")
+print("This is Question 4 and 5");
+print("------------------------");
+age = 31
+if age >= 18:
+    print "Yay! I can vote!";
+else:
+    print "Awww, I cannt vote";
+
+
+print ("\n")
+print("This is Question 6");
+print("------------------------");
+age = input("How old are you? ")
+if (age >= 18) and (age >= 21):
+    print "I can vote and go to a bar!"
+
+
+print ("\n")
+print("This is Question 7");
+print("------------------------");
+age = input("How old are you? ")
+if (age >= 21):
+    print "I can go to a bar.";
+elif (age >= 18) and (age < 21):
+    print "I can vote but I cannot go to a bar";
+else:
+    print "I cannot vote or go to a bar";
+
+
+print ("\n")
+print("This is Question 8");
+print("------------------------");
+number = input("What number do you want to check: ");
+if ((number % 2)==0):
+    print "Number " + str(number) + " is even";
+else:
+    print "Number " + str(number) + " is odd";
+
+#test 8 = even 
+
+
