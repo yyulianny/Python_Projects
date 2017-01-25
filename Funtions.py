0acbbaad2964504dc558d1ac19252a87c94e7271
Yenny Yulianny <yulianny.fang@gmail.com>
2017-01-24 17:53:45 -0800

    Funtions.py

---
+++ Funtions.py
@@ -0,0 +1,27 @@
+def hello_print():
+    print "hello"
+print hello_print()
+
+
+#def hello_print():
+#print hello_print()
+
+
+#def hello_print():
+#    print "hi"
+#print hello_print()
+
+
+#def hello_print():
+#   print "gretting"
+#print hello_print()
+
+
+def hello_return():
+    return "hello"
+print hello_return()
+
+
+def hello_return():
+    return "hello"
+print hello_return()
