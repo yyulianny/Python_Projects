603b07d8717f39a940a1e07074e29d294f615f77
Yenny Yulianny <yulianny.fang@gmail.com>
2017-01-24 20:28:08 -0800

    Update

--- Funtions.py
+++ Funtions.py
@@ -93,5 +93,32 @@ def square(num):
 print square(2)
 
 
+def main():
+    #(4+5) + (9-6)
+    print add(add(4,5), subtract(9,6))
 
+    # (12/2) - 60
+    print subtract(divide(12, 2), 60)
+
+     # 1 + 2 + 3
+    print add(add(1, 2), 3)
+
+    # (1 + 2)^2
+    print square(add(1, 2))
+
+    # (3%4) / (9*9)
+    print divide(modulo(3, 4), multiply(9, 9))
+
+    # 7 * (3+8)
+    print multiply(7, add(3, 8))
+
+    # (1+2) - 3 * (4+5)
+    print subtract(add(1, 2), multiply(3, add(4, 5)))
+
+    # 3^(2+3)
+    print power(3, add(2, 3))
+
+if __name__ == "__main__":
+    print "Show the __name__ of this calculator_main.py module.", __name__
+    main()
     
