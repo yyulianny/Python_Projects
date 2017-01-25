91989f1803f6dd13975674241092279da0b3f7d4
Yenny Yulianny <yulianny.fang@gmail.com>
2017-01-24 19:43:57 -0800

    Functions_Part2

--- Funtions.py
+++ Funtions.py
@@ -17,11 +17,81 @@ print hello_print()
 #print hello_print()
 
 
-def hello_return():
-    return "hello"
-print hello_return()
+#def hello_return():
+#   return "hello"
+#print hello_return()
 
 
-def hello_return():
-    return "hello"
-print hello_return()
+def hello_name():
+    name = "Yenny"
+    print "hello"
+    return "Yenny"
+print hello_name()
+
+
+def hello_name():
+    print "hello_name"
+print hello_name()
+
+
+def hello_name():
+    name = "Maria"
+    return "Maria"
+print hello_name()
+
+
+def hello_name():
+    return "Maria"
+print hello_name()
+
+
+def partner_name():
+    Maria = 77
+    return "partner_name"
+print partner_name()
+
+
+#Funtions Part 2
+
+def add(num1, num2):
+    return num1 + num2
+print add(4,5)
+
+
+def subtract(num1, num2):
+    return num2 - num1
+print subtract(4,5)
+
+
+def multiply(num1, num2):
+    return num1 * num2
+print multiply(4,5)
+
+
+def divide(num1, num2):
+    return num1 / num2
+print divide(4,5)
+
+
+def modulo(num1, num2):
+    return num1 % num2
+print modulo(4,5)
+
+
+def power(base, exponent):
+    return base ** exponent
+print power(4,5)
+
+
+def square(num):
+    return power(num, 2)
+print square(2)
+
+
+def square(num):
+    return power(num, 6)
+print square(2)
+
+
+
+    
